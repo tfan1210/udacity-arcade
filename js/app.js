82849d99a993f9c2ef0b62fe9f9e32182b62e17e
terence.fan <terence.fan@sha-50424-mbp.local>
2018-06-25 16:21:05 +0800

    enemies speed and update location

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+/* define Enemy */
+
+var eRows = [83*1-20, 83*2-20, 83*3-20];  // array defining y-coordinate of stone-rows 1-3 in which Enemy can move
+                                          // row = 83px {28}; -20 to position sprite squarely within row
+                                          // note: image size 101x171; image-height:171 > row-height:83, so top-left corner sits way inside row above
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -18,9 +24,20 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  if(this.x + 101*0.8 >= player.x && this.x  <= player.x && this.y + 83*0.2 >= player.y && this.y - 83*0.2 <= player.y) {
+      // x & y values adjusted for better collision detection; in general, x-position of Enemy = x-position of Player - 10 for better appearence
+      this.score = 0; // add to player score
+      document.getElementById('score').innerHTML = "Score: 0"; // update score upon collision
+      document.getElementById('result').innerHTML = "Ouch! Try again"; // update result upon collision
+      player.reset();  // return player to starting position
+  };
+
+  this.x += this.speed * dt;  // move at rate of 'dt' {43} (time delta between ticks) to ensure game runs at same speed for all computers
+
+  if (this.x > 505) {  // if Enemy goes off-screen [canvas.width = 505 {135}; each column = 101px, each row = 83px {28}]
+      this.x = Math.random() * -400;  // reset position (to initial conditions, see above)
+      this.y = eRows[Math.floor(Math.random() * eRows.length)];
+  };
 };
 
 // Draw the enemy on the screen, required method for game
