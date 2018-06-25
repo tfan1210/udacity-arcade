5c5791e24ec3622cd3e2a316f10a2c5d8f779f5b
terence.fan <terence.fan@sha-50424-mbp.local>
2018-06-25 17:21:38 +0800

    updated player sprite

--- js/app.js
+++ js/app.js
@@ -1,43 +1,27 @@
-/* define Enemy */
-
-var eRows = [83*1-20, 83*2-20, 83*3-20];  // array defining y-coordinate of stone-rows 1-3 in which Enemy can move
-                                          // row = 83px {28}; -20 to position sprite squarely within row
-                                          // note: image size 101x171; image-height:171 > row-height:83, so top-left corner sits way inside row above
-
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-
-    this.x = Math.random() * -500;  // select random horizontal start position (off-screen); changing this value alters how soon Enemy appears
-    this.y = eRows[Math.floor(Math.random() * eRows.length)];  // select random row (vertical position)
-
-    this.speed = Math.random() * 400 + 100;  // speed varies randomly from min 100 to max <500; changing this value alters how fast Enemy moves
-                                             // if game had difficulty levels speed could increase by a factor, e.g 400*a, for a = [1,2,4]
-
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  if(this.x + 101*0.8 >= player.x && this.x  <= player.x && this.y + 83*0.2 >= player.y && this.y - 83*0.2 <= player.y) {
-      // x & y values adjusted for better collision detection; in general, x-position of Enemy = x-position of Player - 10 for better appearence
-      this.score = 0; // add to player score
-      document.getElementById('score').innerHTML = "Score: 0"; // update score upon collision
-      document.getElementById('result').innerHTML = "Ouch! Try again"; // update result upon collision
-      player.reset();  // return player to starting position
-  };
-
-  this.x += this.speed * dt;  // move at rate of 'dt' {43} (time delta between ticks) to ensure game runs at same speed for all computers
-
-  if (this.x > 505) {  // if Enemy goes off-screen [canvas.width = 505 {135}; each column = 101px, each row = 83px {28}]
-      this.x = Math.random() * -400;  // reset position (to initial conditions, see above)
-      this.y = eRows[Math.floor(Math.random() * eRows.length)];
-  };
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x + this.speed * dt;
+    if (this.x >= 505) {
+        this.x = 0;
+    }
+    this.checkCollision();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -45,54 +29,83 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-var Player = function() {
-
-    this.sprite = 'images/char-boy.png';  // load image // why is game crashing if image is changed??
-
-    this.x = 101*2;  // start in 3rd column [image size 101x171; image-width:101 = width:101, so no adjustment needed]
-    this.y = 83*5-10;  // and 6th "row" [see position info in l.9]; -10 offset to position sprite squarely within cell (block)
-
-    this.score = 0; // set initial score = 0
-
-};
-
-Player.prototype.update = function() {
-
-    this.x = this.x;
-    this.y = this.y;
-
-};
-
-Player.prototype.handleInput = function(allowedKeys) { // move Player according to keystrokes
-    switch(allowedKeys) {
-      case 'left':
-        this.x = (this.x - 101 >= 0) ? this.x - 101 : 0;  // if not in leftmost cell, move one cell left, else do nothing
-        break;
-      case 'right':
-        this.x = (this.x + 101 <= 101*4) ? this.x + 101 : 101*4;  // if not in rightmost cell, move one cell right, else do nothing
-        break;
-      case 'down':
-        this.y = (this.y + 83 <= 83*5) ? this.y + 83 : 83*5;  // if not in lowermost cell, move one cell down, else do nothing
-        break;
-      case 'up':
-            if(this.y - 83 >= 60){  // if not in topmost cell,
-                this.y -= 83;       // move one cell up,
-            } else {
-
-                this.score += 100;  // else: add to score
-                document.getElementById('score').innerHTML = "Your score: " + this.score; // update score upon success
-                document.getElementById('result').innerHTML = "You made it!"; // update result upon success
-
-                this.reset();  // return player to starting position
-            }
-        break;
-      default:
-        console.log('default');
-        break;
+// checks en enemy's collision with player
+Enemy.prototype.checkCollision = function() {
+    if (player.y + 131 >= this.y + 90 &&
+        player.y + 73 <= this.y + 135 &&
+        player.x + 25 <= this.x + 88 &&
+        player.x + 76 >= this.x + 11) {
+        console.log('collision');
+        gameReset();
     }
 };
 
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-horn-girl.png';
+};
+
+// Update method for Player
+Player.prototype.update = function() {};
+
+// renders the player
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+/*
+ * handles input for the player
+ */
+Player.prototype.handleInput = function(key) {
+    if (key == 'left') {
+        this.x = (this.x - this.speed + 505) % 505;
+    } else if (key == 'right') {
+        this.x = (this.x + this.speed) % 505;
+    } else if (key == 'up') {
+        this.y = (this.y - this.speed + 606) % 606;
+        // going to water
+        if (this.y <= (83 - 48)) { // line 135 engine.js
+            // assuming 48 to be player height
+            gameOver();
+            return;
+        }
+    } else {
+        this.y = (this.y + this.speed) % 606;
+        if (this.y > 400) { // bottom limit
+            this.y = 400;
+        }
+    }
+    // x axis wrap
+    if (this.x < 2.5) {
+        this.x = 2.5;
+    }
+    if (this.x > 458) {
+        this.x = 458;
+    }
+};
+
+/*
+ * resets the player to default position
+ */
+Player.prototype.reset = function() {
+    this.x = 202.5;
+    this.y = 383;
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+var player = new Player(0, 0, 50);
+var scoreDiv = document.createElement('div');
+gameReset(); // setup defaults
+var canvasDiv = document.getElementsByTagName('canvas')[0];
+document.body.insertBefore(scoreDiv, canvasDiv);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -107,17 +120,39 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// draw player on-screen
+// custom
+// increase enemies by score
+var score = 0;
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+/*
+ * resets the game in case of collision
+ */
+function gameReset() {
+    player.reset();
+    score = 0;
+    updateDisplay();
+    allEnemies = [];
+    allEnemies.push(
+        new Enemy(0, Math.random() * 150 + 50, Math.random() * 100 + 40),
+        new Enemy(0, Math.random() * 150 + 70, Math.random() * 100 + 60)
+    );
+}
 
- // Player will reset after collision
+/*
+ * game over successfully (reached water)
+ */
+function gameOver() {
+    player.reset();
+    score += 1;
+    updateDisplay();
+    if (score % 2 == 0 && allEnemies.length < 4) {
+        allEnemies.push(new Enemy(0, Math.random() * 160 + 50, Math.random() * 90 + 70));
+    }
+}
 
- Player.prototype.reset = function() {  // reset Player
-
-    this.x = 101*2;  // start in 3rd "column"
-    this.y = 83*5-10;  // and 6th "row"
-
+/*
+ * updates the on screen score display
+ */
+function updateDisplay() {
+    scoreDiv.innerHTML = 'Score ' + score;
 }
